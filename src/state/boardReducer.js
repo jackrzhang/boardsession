03f5c68dcd1383138f090a0de110a466433364c9
Jack Zhang <15jzhang@gmail.com>
2016-12-03 17:58:48 -0500

    Add current user id to client side state

--- src/state/boardReducer.js
+++ src/state/boardReducer.js
@@ -1,11 +1,10 @@
 import { Map } from 'immutable';
 
-import {
-  START_LINE,
-  END_LINE
-} from './../client/app/canvas/canvasActions';
+import { START_LINE, END_LINE } from './../client/app/canvas/canvasActions';
+import { CONNECT_USER } from './../client/socket/socketActions';
 
 export const initialBoard = Map({
+  userId: null,
   isDrawing: false
 });
 
@@ -15,6 +14,8 @@ const boardReducer = (state = initialBoard, action) => {
       return state.set('isDrawing', true);
     case END_LINE:
       return state.set('isDrawing', false);
+    case CONNECT_USER:
+      return state.set('userId', action.userId);
     default:
       return state;
   }
