073e63972982a7ba3d396b5b57781bcb937975ba
Jack Zhang <15jzhang@gmail.com>
2016-12-08 03:20:06 -0500

    Implement Mode component container

--- src/client/app/toolbar/mode/Mode.container.js
+++ src/client/app/toolbar/mode/Mode.container.js
@@ -1,13 +1,16 @@
 import { connect } from 'react-redux';
 import Mode from './Mode.view';
 
+import { changeMode } from './../toolbarActions';
 
 const mapStateToProps = state => ({
-  
+  mode: state.get('marker').get('mode')
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  changeMode: (color) => {
+    dispatch(changeMode(color));
+  }
 });
 
 export default connect(
