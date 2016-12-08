9a024fcf6cf762ae7a335daf642171a12f4e05f5
Jack Zhang <15jzhang@gmail.com>
2016-12-08 03:21:55 -0500

    Add mode field to marker state, add reducer case for CHANGE_MODE

--- src/state/markerReducer.js
+++ src/state/markerReducer.js
@@ -1,22 +1,30 @@
 import { Map } from 'immutable';
 
 import {
+  BLACK,
   SIZE_3,
-  BLACK
+  DRAW
 } from './markerConstants';
 
 export const initialMarker = Map({
+  color: BLACK,
   size: SIZE_3,
-  color: BLACK
+  mode: DRAW
 });
 
-import { CHANGE_COLOR, CHANGE_SIZE } from './../../src/client/app/toolbar/toolbarActions';
+import {
+  CHANGE_COLOR,
+  CHANGE_SIZE,
+  CHANGE_MODE
+} from './../../src/client/app/toolbar/toolbarActions';
 
 const markerReducer = (state = initialMarker, action) => {
   switch (action.type) {
     case CHANGE_COLOR:
       return state.set('color', action.color);
     case CHANGE_SIZE:
+      return state.set('size', action.size);
+    case CHANGE_MODE:
       return state.set('size', action.size);
     default:
       return state;
