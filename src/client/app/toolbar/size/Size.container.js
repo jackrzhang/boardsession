062260bc02332c7320a429bcdf92461fb16a5b48
Jack Zhang <15jzhang@gmail.com>
2016-12-08 02:49:07 -0500

    Implement Size container component

--- src/client/app/toolbar/size/Size.container.js
+++ src/client/app/toolbar/size/Size.container.js
@@ -1,13 +1,16 @@
 import { connect } from 'react-redux';
 import Size from './Size.view';
 
+import { changeSize } from './../toolbarActions';
 
 const mapStateToProps = state => ({
-
+  size: state.get('marker').get('size')
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  changeSize: (size) => {
+    dispatch(changeSize(size));
+  }
 });
 
 export default connect(
